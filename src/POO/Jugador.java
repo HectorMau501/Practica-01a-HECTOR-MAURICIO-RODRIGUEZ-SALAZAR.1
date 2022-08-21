247131e8bfb815514821eab76bcd8b57cd3a1ab3
HectorMau501 <mauhector7@gmail.com>
2022-08-20 23:09:38 -0500

    Parte dos

--- src/POO/Jugador.java
+++ src/POO/Jugador.java
@@ -2,13 +2,64 @@
 package POO;
 
 //Jugador es la clase Padre o super 
-public class Jugador {
+public class Jugador 
+{
     
     //Metodos
     private String nombre;
-    private String codigo;
-    private int nivelExperiencia;
-    private 
+    private int codigo;
+    private float tiempoJugando;
+    private String modalidad; 
+
+    //Constructor
+    public Jugador(String nombre, int codigo, float tiempoJugando, String modalidad)
+    {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.tiempoJugando = tiempoJugando;
+        this.modalidad = modalidad;
+    }
     
+    //Metodos Setters y Getters
     
+     public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+    
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setCodigo(int codigo)
+    {
+        this.codigo = codigo;
+    }
+       
+    public int getCodigo() 
+    {
+        return codigo;
+    }
+
+    public void setTiempoJugando(float tiempoJugando)
+    {
+        this.tiempoJugando = tiempoJugando;
+    }
+    
+    public float getTiempoJugando() 
+    {
+        return tiempoJugando;
+    }
+    
+    public void setModalidad(String modalidad)
+    {
+        this.modalidad = modalidad;
+    }
+    
+    public String getModalidad() 
+    {
+        return modalidad;
+    } 
+       
 }
--- src/POO/Personaje.java
+++ src/POO/Personaje.java
@@ -2,8 +2,64 @@
 package POO;
 
 //Metodos de la clase hija
-public class Personaje extends Jugador{
+public class Personaje extends Jugador
+{
     
     //Metodos de la clase hija
     
+    private String genero;
+    private float altura;
+    private String habilidad;
+    private int nivelExperiencia;
+
+    public Personaje(String genero, float altura, String habilidad, int nivelExperiencia, String nombre, int codigo, float tiempoJugando, String modalidad)
+    {
+        super(nombre, codigo, tiempoJugando, modalidad);
+        this.genero = genero;
+        this.altura = altura;
+        this.habilidad = habilidad;
+        this.nivelExperiencia = nivelExperiencia;
+    }
+    
+    //  Metodos Setters y Getters de la clase hija
+    
+    public void setGenero(String genero) 
+    {
+        this.genero = genero;
+    }
+
+    public String getGenero()
+    {
+        return genero;
+    }
+
+    public void setAltura(float altura)
+    {
+        this.altura = altura;
+    }
+    
+    public float getAltura() 
+    {
+        return altura;
+    }
+    
+    public void setHabilidad(String habilidad) 
+    {
+        this.habilidad = habilidad;
+    }
+
+    public String getHabilidad() {
+        return habilidad;
+    }
+    
+    public void setNivelExperiencia(int nivelExperiencia) 
+    {
+        this.nivelExperiencia = nivelExperiencia;
+    }
+    
+    public int getNivelExperiencia() 
+    {
+        return nivelExperiencia;
+    }
+   
 }
